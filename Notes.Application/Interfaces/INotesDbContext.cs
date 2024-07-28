39cb65884fb359234a9d9bf941e4ae9cfe0b17cb
whoami <az.khabibullin2018@gmail.com>
2024-07-28 22:42:45 +0500

    урок 3 добавление бд в проект
    автор использует sqlite, но я буду на sql server
    
    конфигурация для сущности note - описание наборов правил и ограничений для сущности
    
    в еф кор, конфигурацию можно задать через аннотацию данных или флюент апи
    аннотация - на поля сущности навешиваются атрибуты
    флюент - набор методов, которые определяют сопоставление между классами и их свойствами, таблицами и столбацами
    
    будет через флюент, код легче читать и он гибкий
    если будут новые сущности, то будет удобно быстро расширять конфигурации для них
    класс реализующий IEntityTypeConfiguration, позволяет разделять конфиг для типов сущности на отдельный класс, а не в onModelCreating. Это сделано чтобы уменьшить колво кода и улучшить читаемость
    
    контекст БД
    интерфейс контекста
    
    в mvc, используют паттерн репозиторий, инкапсуляция логики работы с БД
    не будет этого
    ef core изолирует код от изменения БД
    dbcontext как unit of work
    dbset как репозиторий
    ef core тесты без репозиториев
    
    dbinitializer, проверяет есть ли БД, если нет то создаст

---
+++ Notes.Application/Interfaces/INotesDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Notes.Domain;
+
+namespace Notes.Application.Interfaces
+{
+    public class INotesDbContext
+    {
+        //все сущности
+        DbSet<Note> Notes { get; set; }
+
+        //дублирование метода
+        //сохранения контекста в БД
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
---
+++ Notes.Persistence/EntityTypeConfigurations/NoteConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Notes.Domain;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Notes.Persistence.EntityTypeConfigurations
+{
+    public class NoteConfiguration : IEntityTypeConfiguration<Note>
+    {
+        public void Configure(EntityTypeBuilder<Note> builder)
+        {
+            //айди задали
+            builder.HasKey(note => note.Id);
+
+            //уникальное название
+            builder.HasIndex(note => note.Id).IsUnique();
+
+            //макс длина
+            builder.Property(note => note.Title).HasMaxLength(250);
+        }
+    }
+}
