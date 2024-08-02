1635fbbc383ac1a4fc8ba7160b82d14e470bbf3f
whoami <az.khabibullin2018@gmail.com>
2024-08-02 21:27:56 +0500

    урок 8 валидация данных

--- Notes.Application/DependencyInjection.cs
+++ Notes.Application/DependencyInjection.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Text;
 using MediatR;
 using System.Reflection;
+using FluentValidation;
+using Notes.Application.Common.Behaviors;
 
 namespace Notes.Application
 {
@@ -14,7 +16,13 @@ namespace Notes.Application
             this IServiceCollection services
             )
         {
-            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly()); 
+            services.AddValidatorsFromAssemblies(
+                new[] { 
+                    Assembly.GetExecutingAssembly() 
+                });
+            services.AddTransient(typeof(IPipelineBehavior<,>),
+                typeof(ValidationBehavior<,>));
             return services;
         }
         
